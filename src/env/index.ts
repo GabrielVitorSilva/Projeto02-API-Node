1fb00caf98b6609600d222821a0a67861d753b05
GabrielVitorSilva <gabrieldatas2004@gmail.com>
2023-06-27 09:47:50 -0300

    Fix: database url

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,7 +3,11 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATABASE_URL: z.string().default('./db/app.db'),
+  DATABASE_URL: z
+    .string()
+    .default(
+      'postgres://ignite_nodejs_02_db_vpdm_user:JQOG1zPf9SusyS2xpUtpTCDFF8RtmxIE@dpg-cicuh3tph6em8s7rrdog-a/ignite_nodejs_02_db_vpdm',
+    ),
   DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('pg'),
   PORT: z.coerce.number().default(3333),
 })
